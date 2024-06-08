f7a7131ba46392234a4e69be83daf70fb0d2b193
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2024-06-08 15:59:36 +0900

    feat : 사이드 바 아이콘 추가

--- gitdeco/src/app/_components/common/Sidebar/index.tsx
+++ gitdeco/src/app/_components/common/Sidebar/index.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import React, { useState } from "react";
 import * as S from "./style";
+import { RiProfileLine } from "react-icons/ri";
+import { FaRegAddressCard } from "react-icons/fa";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
 const Sidebar = () => {
+  const [selectedIcon, setSelectedIcon] = useState<string>("");
+
+  const handleIconClick = (icon: string) => {
+    setSelectedIcon(icon);
+  };
+
+  const getIconProps = (icon) => ({
+    color: selectedIcon === icon ? "#E7E7E8" : "#909294",
+    size: selectedIcon === icon ? 38 : 34,
+    onClick: () => handleIconClick(icon),
+    style: { transform: selectedIcon === icon ? "scale(1.1)" : "scale(1)" },
+  });
+
   return (
     <S.SidebarContainer>
-      <div>hello</div>
+      <S.IconWraper>
+        <FaRegAddressCard {...getIconProps("FaRegAddressCard")} />
+        <RiProfileLine {...getIconProps("RiProfileLine")} />
+        <AiOutlineFundProjectionScreen
+          {...getIconProps("AiOutlineFundProjectionScreen")}
+        />
+      </S.IconWraper>
     </S.SidebarContainer>
   );
 };
--- gitdeco/src/app/_components/common/Sidebar/style.ts
+++ gitdeco/src/app/_components/common/Sidebar/style.ts
@@ -8,4 +8,24 @@ export const SidebarContainer = styled.div`
   height: 92vh;
   background-color: black;
   border-right: 1px solid #3d4145;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const IconWraper = styled.div`
+  margin-top: 5vh;
+  height: 20vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  & > * {
+    margin: 10px 0;
+    transition:
+      color 0.3s ease,
+      transform 0.3s ease;
+  }
 `;
