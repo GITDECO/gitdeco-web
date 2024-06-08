2b0ee9b9b93cd8391b225394a71fa59d86b05c77
김수인 <kim061222@gmail.com>
2024-06-09 01:50:19 +0900

    feat : Web Header 개발

--- gitdeco/src/app/_components/common/Header/index.tsx
+++ gitdeco/src/app/_components/common/Header/index.tsx
@@ -1,9 +1,17 @@
 import * as S from "./style";
+import Image from "next/image";
+import LogoIco from "/public/gitdeco.svg";
 
 const Header = () => {
   return (
     <S.HeaderContainer>
-      <div>hello</div>
+      <S.Logo>
+        <S.LogoImage>
+          <Image src={LogoIco} alt="Logo Img" layout="fill" objectFit="contain" />
+        </S.LogoImage>
+        <S.LogoTxt>GitDeco</S.LogoTxt>
+      </S.Logo>
+      <S.DownloadBtn>Download</S.DownloadBtn>
     </S.HeaderContainer>
   );
 };
--- gitdeco/src/app/_components/common/Header/style.ts
+++ gitdeco/src/app/_components/common/Header/style.ts
@@ -8,4 +8,49 @@ export const HeaderContainer = styled.div`
   background-color: black;
   box-sizing: border-box;
   border-bottom: 1px solid #3d4145;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 3vw;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const LogoTxt = styled.div`
+  color: white;
+  font-weight: 500;
+  font-size: 1.5vw;
+  margin-top: 0.5vw;
+  margin-left: 0.5vw;
+`;
+
+export const LogoImage = styled.div`
+  position: relative;
+  width: 50px;
+  height: 50px;
+`;
+
+export const DownloadBtn = styled.button`
+  width: 7vw;
+  height: 1vh;
+  padding: 15px;
+  background-color: #2B64E7;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size:1vw;
+  margin-bottom: 0.5vw;
+
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  
+  &:hover {
+    background-color: #6062AA;
+  }
 `;
